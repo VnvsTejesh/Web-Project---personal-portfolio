f7c53159753211a058847ddebe06ea2f2e5cd6b8
Tejesh <nvuddag2@pdx.edu>
2023-03-17 06:44:20 -0700

    changes for responsiveness in work experience section and some more touch up

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-let btnMenu = document.getElementById("btn-menu");
-let menu = document.getElementById("menu");
+const btnMenu = document.getElementById("btn_menu");
+const menu = document.getElementById("menu");
 
-btnMenu.onclick = () => {
+btnMenu.addEventListener("click", () => {
   btnMenu.classList.toggle("fa-times");
   menu.classList.toggle("active");
-};
+});
+
+
+// ============================================
+
+const form = document.getElementsByClassName("contact-form")[0];
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const { value: name } = document.getElementById("username");
+  const { value: email } = document.getElementById("email");
+  const { value: message } = document.getElementById("text");
+  if (name.length === 0 && email.length === 0) {
+    console.warn("You must enter some data to submit this form");
+  } else {
+    const formattedName = name.length === 0 ? "no submission" : name;
+    const formattedEmail = email.length === 0 ? "no submission" : email;
+    const formattedText = message.length === 0 ? "no submission" : email;
+    console.log("======== Form Submission =========");
+    console.log(`   Username: ${formattedName}`);
+    console.log(`   Email: ${formattedEmail}`);
+    console.log(`   Message: ${formattedText}`);
+  }
+});
